d148effa33d2a06941c5a79841ffb0925366b641
dilhara19 <dilharas84@gmail.com>
2026-03-13 05:31:21 +0530

    Fitness Center Membership System Project

---
+++ fitness-center-system/src/main/java/com/fitness/fitnesscentersystem/FitnessCenterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnesscentersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCenterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessCenterSystemApplication.class, args);
+    }
+
+}
---
+++ fitness-center-system/src/test/java/com/fitness/fitnesscentersystem/FitnessCenterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fitnesscentersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCenterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
